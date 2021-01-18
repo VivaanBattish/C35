0fea598e84cddb33ec893db78f9606b13e39de64
VivaanBattish <vivanbattish@gmail.com>
2021-01-18 19:10:43 +0530

    C35

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
 var ball;
+var database,position; 
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+  var  bposition = database.ref("ball/position");
+
+   bposition.on("value",changes,errors);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
+}
+function errors(){
+    console.log("error");
+}
+
+function changes(data){
+position = data.val();
+console.log(position);
+ball.x = position.x;
+ball.y = position.y;
 }
 
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x: ball.x + x,
+        y: ball.y + y,
+    })
 }
